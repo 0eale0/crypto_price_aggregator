1edee4bbd03e53c1fc7d2d0be3f2d32cd7357b6f
daniskazan <danis.sharafiev@bk.ru>
2022-04-24 23:44:41 +0300

    add binance-api

---
+++ app/api/binance-api.py
@@ -0,0 +1,16 @@
+from binance.client import Client
+
+def cryptocurrencies_usdt_price() -> list:
+    """
+    Возвращает отсортированные пары по цене в долларах от большего к меньшему
+    """
+    result = list()
+    c = Client()
+    tickers = c.get_all_tickers()
+    for coin in tickers:
+        if "USDT" in coin['symbol']:
+            result.append(coin)
+        else:
+            continue
+    return list(filter(lambda x: max(x['price']), result))
+
