eccf0a052747e0600ab83534314d63c921a4a6f6
eale <RUSLIKPVP@yandex.ru>
2022-04-29 20:06:37 +0300

    add changes to merge

--- app/models/forms/users.py
+++ app/models/forms/users.py
@@ -1,4 +1,19 @@
-from pydantic import EmailStr, BaseModel
+from pydantic import EmailStr, BaseModel, validator
+
+
+class UserValidation:
+    @validator("username")
+    def check_username(cls, v):
+        if not len(v) >= 3:
+            raise ValueError("Username should be at least 3 symbols")
+
+        return v.title()
+
+    @validator("password")
+    def password_len(cls, v, values, **kwargs):
+        if not len(v) >= 4:
+            raise ValueError("Password length should be at least 4 symbols")
+        return v
 
 
 class RegistrationForm(BaseModel):
@@ -9,35 +24,47 @@ class RegistrationForm(BaseModel):
 
     async def load_data(self):
         form = await self.request.form()
-        self.username = form.get('username')
-        self.email = form.get('email')
-        self.password = form.get('password')
+        print(form)
+        self.username = form.get("username")
+        self.email = form.get("email")
+        self.password = form.get("password")
 
-    def username_length_is_valid(self) -> bool or dict:
-        return len(self.username) >= 3
+    @validator("username")
+    def check_username(cls, v):
+        if not len(v) >= 3:
+            raise ValueError("Username should be at least 3 symbols")
 
-    def password_length_is_valid(self):
-        return len(self.password) >= 4
+        return v.title()
 
-    def passwords_equal_is_valid(self):
-        return self.password == self.repeat_password
+    @validator("password")
+    def password_len(cls, v, values, **kwargs):
+        if not len(v) >= 4:
+            raise ValueError("Password length should be at least 4 symbols")
+        return v
+
+    @validator("repeat_password")
+    def passwords_match(cls, v, values, **kwargs):
+        if "password" in values and v != values["password"]:
+            raise ValueError("passwords do not match")
+        return v
 
 
 class ChangeDataForm(BaseModel):
     username: str
-    new_username: str
-    email: EmailStr
     password: str
-    new_password: str
 
     async def load_data(self):
         form = await self.request.form()
-        self.username = form.get('username')
-        self.email = form.get('email')
-        self.password = form.get('password')
 
-    def username_length_is_valid(self) -> bool or dict:
-        return (len(self.username) >= 3) and (len(self.new_username) >= 3 or len(self.new_username) >= 0)
+    @validator("username")
+    def check_username(cls, v):
+        if not len(v) >= 3:
+            raise ValueError("Username should be at least 3 symbols")
 
-    def password_length_is_valid(self):
-        return (len(self.password) >= 4) and (len(self.new_password) >= 4 or len(self.new_password) == 0)
+        return v.title()
+
+    @validator("password")
+    def password_len(cls, v, values, **kwargs):
+        if not len(v) >= 4:
+            raise ValueError("Password length should be at least 4 symbols")
+        return v
