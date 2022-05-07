d9c52f19ac39fe0a773ff837a8f449265dd00fc8
Adelya <adelya6387240@gmail.com>
2022-05-07 21:24:33 +0300

    Add delete_favourite_crypto with form

--- app/api/routes/top_10.py
+++ app/api/routes/top_10.py
@@ -9,6 +9,8 @@ from app.models.domain.users import CoinPrice, UserFavouriteCrypto, Cryptocurren
 from app.models.forms.users import NameFavouriteCryptoForm
 
 router = APIRouter()
+
+
 # asc возрастающ
 
 
@@ -17,10 +19,10 @@ def top_10_most_expensive(db: Session = Depends(get_session)):
     coins = [
         c.dumps()
         for c in db.query(Cryptocurrency)
-        .order_by(desc(Cryptocurrency.price))
-        .order_by(desc(Cryptocurrency.time))
-        .limit(10)
-        .all()
+            .order_by(desc(Cryptocurrency.price))
+            .order_by(desc(Cryptocurrency.time))
+            .limit(10)
+            .all()
     ]
     return coins
 
@@ -30,15 +32,15 @@ def top_10_cheapest(db: Session = Depends(get_session)):
     coins = [
         c.dumps()
         for c in db.query(Cryptocurrency)
-        .order_by(asc(Cryptocurrency.price))
-        .order_by(desc(Cryptocurrency.time))
-        .limit(10)
-        .all()
+            .order_by(asc(Cryptocurrency.price))
+            .order_by(desc(Cryptocurrency.time))
+            .limit(10)
+            .all()
     ]
     return coins
 
 
-@router.post("/favourite_crypto")
+@router.post("/add_favourite_crypto")
 def add_favourite_crypto_in_db(request: Request, form: NameFavouriteCryptoForm, db: Session = Depends(get_session)):
     try:
         current_user = request.session.get("user")
@@ -63,8 +65,29 @@ def add_favourite_crypto_in_db(request: Request, form: NameFavouriteCryptoForm, 
         return str(e)
 
 
+@router.post("/delete_favourite_crypto")
+def delete_favourite_crypto_in_db(request: Request, form: NameFavouriteCryptoForm, db: Session = Depends(get_session)):
+    try:
+        current_user = request.session.get("user")
+        user = db.query(User).filter(User.username == current_user["username"]).first()
+        if user:
+            if form.name_crypto:
+                coin = db.query(Cryptocurrency).filter(Cryptocurrency.symbol == form.name_crypto).first()
+                user_with_fav_crypto = db.query(UserFavouriteCrypto).filter(UserFavouriteCrypto.user_id == user.id).filter(UserFavouriteCrypto.coin_id == coin.id).first()
+                db.delete(user_with_fav_crypto)
+                db.commit()
+                return user_with_fav_crypto
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authorized",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    except Exception as e:
+        return str(e)
+
+
 @router.get("/get_favourite_crypto")
-def add_favourite_crypto_in_db(request: Request, db: Session = Depends(get_session)):
+def get_favourite_crypto_in_db(request: Request, db: Session = Depends(get_session)):
     try:
         current_user = request.session.get("user")
         user = db.query(User).filter(User.username == current_user["username"]).first()
