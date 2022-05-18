d1009b6ea6edf1637937db07ad3f8228622a0d95
Adelya <adelya6387240@gmail.com>
2022-05-18 14:24:25 +0300

    Add update tests on parametrize

--- app/tests/test_auth_helpers.py
+++ app/tests/test_auth_helpers.py
@@ -1,13 +1,18 @@
 from app.api.services.auth_helpers import verify_password, get_password_hash
+import pytest
 
 
-def test_verify_password():
-    assert verify_password('string', '$2b$12$Bp0qpiUX/Mk0tl9YGteRzeTDCiaVQhobQpYYTvS28ybhQLgXUmuyi') is True
-    assert verify_password('894579', '$2b$12$zSn4mklK3aQ8sTwk.r2Ws.hxpBrTCCH5yWI1xkJDRgf8WtZDT5tUq') is True
-    assert verify_password('184509', '$2b$12$9kOVp/MvokQuGG0YvXty.OQ5LIRnua7slGm6SlVBm7uYGAHSbw6eS') is False
-    assert type(verify_password('184509', '$2b$12$9kOVp/MvokQuGG0YvXty.OQ5LIRnua7slGm6SlVBm7uYGAHSbw6eS')) == bool
+@pytest.mark.parametrize(
+    'input_data, output_data, expected',
+    [
+        ('string', '$2b$12$Bp0qpiUX/Mk0tl9YGteRzeTDCiaVQhobQpYYTvS28ybhQLgXUmuyi', True),
+        ('894579', '$2b$12$zSn4mklK3aQ8sTwk.r2Ws.hxpBrTCCH5yWI1xkJDRgf8WtZDT5tUq', True),
+        ('184509', '$2b$12$9kOVp/MvokQuGG0YvXty.OQ5LIRnua7slGm6SlVBm7uYGAHSbw6eS', False)
+    ]
+)
+def test_true_false_verify_password(input_data, output_data, expected):
+    assert verify_password(input_data, output_data) == expected
 
 
 def test_get_password_hash():
     assert type(get_password_hash('456789')) == str
-
--- app/tests/test_forms_users.py
+++ app/tests/test_forms_users.py
@@ -1,7 +1,16 @@
+import pytest
+
 from app.models.forms.users import UserValidation
 
 
-def test_check_username():
+@pytest.mark.parametrize(
+    'input_data, expected',
+    [
+        ('Ivan', 'Ivan'),
+        ('Sasha', 'Sasha'),
+        ('Peter', 'Peter')
+    ]
+)
+def test_check_username(input_data, expected):
     assert UserValidation.check_username('Ivan') == 'Ivan'
     assert UserValidation.check_username('Sasha') == 'Sasha'
-    assert type(UserValidation.check_username('Peter')) == str
