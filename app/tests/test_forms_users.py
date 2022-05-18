1a50d47277ddad4616f17e5fc98f6f1e63dfb9ca
Adelya <adelya6387240@gmail.com>
2022-05-18 15:04:41 +0300

    fix commit

--- app/tests/test_forms_users.py
+++ app/tests/test_forms_users.py
@@ -4,36 +4,23 @@ from app.models.forms.users import UserValidation, RegistrationForm, ChangeDataF
 
 
 @pytest.mark.parametrize(
-    'input_data, expected',
-    [
-        ('Ivan', 'Ivan'),
-        ('Sasha', 'Sasha'),
-        ('Peter', 'Peter')
-    ]
+    "input_data, expected", [("Ivan", "Ivan"), ("Sasha", "Sasha"), ("Peter", "Peter")]
 )
 def test_check_username_in_user_validation(input_data, expected):
     assert UserValidation.check_username(input_data) == expected
 
 
 @pytest.mark.parametrize(
-    'input_data, expected',
-    [
-        ('Adelya', 'Adelya'),
-        ('Ruslan', 'Ruslan'),
-        ('Danis', 'Danis')
-    ]
+    "input_data, expected",
+    [("Adelya", "Adelya"), ("Ruslan", "Ruslan"), ("Danis", "Danis")],
 )
 def test_check_username_in_register_form(input_data, expected):
     assert RegistrationForm.check_username(input_data) == expected
 
 
 @pytest.mark.parametrize(
-    'input_data, expected',
-    [
-        ('Masha', 'Masha'),
-        ('Artem', 'Artem'),
-        ('Kamila', 'Kamila')
-    ]
+    "input_data, expected",
+    [("Masha", "Masha"), ("Artem", "Artem"), ("Kamila", "Kamila")],
 )
 def test_check_username_in_change_data_form(input_data, expected):
     assert ChangeDataForm.check_username(input_data) == expected
