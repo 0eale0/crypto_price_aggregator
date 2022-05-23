a81713ff29a95eabc52c11712886e30fb8658332
Adelya <adelya6387240@gmail.com>
2022-05-23 16:05:56 +0300

    Update docstrings

--- app/api/crypto_sites/ftx_api.py
+++ app/api/crypto_sites/ftx_api.py
@@ -7,7 +7,14 @@ from pprint import pprint
 class FTXApi(CryptoSiteApi):
     name = "ftx"
 
-    async def get_coin_price_from_api(self, symbol: str):
+    async def get_coin_price_from_api(self, symbol: str) -> dict | None:
+        """
+        Requests ftx_api json.
+        From this json gets information about the cryptocurrency (symbol and price),
+        the name of which is fed to the input of the function,
+        and returns it in the form of a dictionary.
+        In case of an error, it terminates the function.
+        """
         try:
             async with aiohttp.ClientSession() as session:
                 url = "https://ftx.com/api/markets/"
